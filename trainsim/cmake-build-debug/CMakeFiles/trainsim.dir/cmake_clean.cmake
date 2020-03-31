131a476d354eda86ee0b460a04f5509dcb501b0d
Martin Kjeldgaard Nikolajsen <mart56p7@stud.kea.dk>
2020-03-31 10:39:26 +0200

    2m

---
+++ trainsim/cmake-build-debug/CMakeFiles/trainsim.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trainsim.dir/main.c.obj"
+  "libtrainsim.dll.a"
+  "trainsim.exe"
+  "trainsim.exe.manifest"
+  "trainsim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/trainsim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ trainsim/cmake-build-debug/CMakeFiles/trainsim.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for trainsim.
+# This may be replaced when dependencies are built.
